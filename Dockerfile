e49e709e3e84c733150ccd21b2cbe8bd8ca94c86
yann <jeanmaireyann.pro@gmail.com>
2023-07-16 23:50:42 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
 # Use an official Node.js runtime as the base image
-FROM node:19.5.0-alpine
+FROM node:19.5.0-alpine AS builder
 
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
-EXPOSE 4200
-CMD ["npm", "start"]
+
+# Use an official Nginx image as the base image
+FROM nginx:latest
+
+# Copy the built static files from the previous stage
+COPY --from=builder /app/dist /usr/share/nginx/html
+
+# Expose port 80 (default for Nginx)
+EXPOSE 80
+
+# The default command for the Nginx image starts Nginx automatically
+CMD ["nginx", "-g", "daemon off;"]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
-import {NgOptimizedImage} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from "@angular/forms";
+import {FlatpickrModule} from "angularx-flatpickr";
+import {DateAdapter} from "@angular/material/core";
+import {CalendarModule} from "angular-calendar";
+import {adapterFactory} from "angular-calendar/date-adapters/moment";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingComponent
+    LandingComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule,
+    CalendarModule,
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
