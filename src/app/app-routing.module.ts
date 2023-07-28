f447f430830160d409f5a4adb5ee0ab4d6f55fe8
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-07-28 13:26:48 +0200

    sso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import {RouterModule, Routes} from "@angular/router";
 import {AboutmeComponent} from "./aboutme/aboutme.component";
 import {LandingComponent} from "./landing/landing.component";
 import {CalendrierComponent} from "./calendrier/calendrier.component";
+import {AuthGuard} from "./authGuard";
 
 const routes: Routes = [
   // /calendar will load the CalendarComponent
-  {path:'about', component: AboutmeComponent},
-  {path:'calendrier', component: CalendrierComponent},
+  {path:'about', component: AboutmeComponent,canActivate: [AuthGuard]
+  },
+
+  {path:'calendrier', component: CalendrierComponent,canActivate: [AuthGuard]},
   // /home will load the LandingComponent
-  {path:'home', component: LandingComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', component: LandingComponent,canActivate: [AuthGuard]},
+  {path: '', redirectTo: '/home', pathMatch: 'full',canActivate: [AuthGuard]},
 ];
 
 
